032510427c0e84f32cdd63f9cbe3b2b39d29bacb
ErmolaevVlad <ermolaev.vlad.0109@gmail.com>
2022-09-19 14:30:23 +0700

    1. Чтение кода и базовые ошибки [#40205]

--- src/main/java/ru/job4j/loop/Counter.java
+++ src/main/java/ru/job4j/loop/Counter.java
@@ -27,7 +27,6 @@ public class Counter {
         System.out.println(sum(2, 15));
         System.out.println(sum(5, 7));
         System.out.println(sum(6, 9));
-
         System.out.println(sumByEven(0, 10));
         System.out.println(sumByEven(3, 8));
         System.out.println(sumByEven(1, 1));
